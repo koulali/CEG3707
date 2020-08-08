79c8ddf5a207ebb07da12f0237420311d864e244
koulali <achraf.koulali@ncl.ac.uk>
2020-08-08 13:31:06 +0100

    read tidegauge data from url

--- ceg3707/psmsl.py
+++ ceg3707/psmsl.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 import os
 
@@ -9,10 +8,15 @@ def load_rlr(tgid):
     Args:
         * tgid  : tide guage id
     '''
-    DATA_DIR = os.path.join(os.path.dirname(__file__), '..')     
-    tgfile = os.path.join(DATA_DIR,'./data/'+tgid+'.rlrdata')
+    local_data = False
+    if (local_data):
+        DATA_DIR = os.path.join(os.path.dirname(__file__), '..')     
+        tgfile = os.path.join(DATA_DIR,'./data/'+tgid+'.rlrdata')
 
-    df = pd.read_csv(tgfile, header=None, sep=";",na_values=-99999,
+    # we use the urllib to download the rlr files
+    url = 'https://www.psmsl.org/data/obtaining/rlr.monthly.data/'+tgid+'.rlrdata'
+
+    df = pd.read_csv(url, header=None, sep=";",na_values=-99999,
                    index_col='decyr', names=['decyr', 'msl', 'nmd', 'flag'])
     
     # remove missing data
